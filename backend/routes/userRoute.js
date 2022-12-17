7850416d7653cfeabb65258a2810354e5b273253
ducusinabdul <ducusinabdul@gmail.com>
2022-12-17 15:23:33 +0800

    update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,12 +2,47 @@ const router = require("express").Router();
 const userModel = require('../models/User.js');
 const bcrypt = require('bcrypt');
 
+// Update User
+router.put('/:id', async (req, res)=> {
+	if(req.body.userId === req.params.id || req.body.isAdmin === true) {
+		if(req.body.password) {
+			try {
+				const salt = await bcrypt.genSalt(10);
+				req.body.password = await bcrypt.hash(req.body.password, salt);
+			} catch(err) {
+				res.status(500).json(err)
+			}
+		}
+		try {
+			const user = await userModel.findByIdAndUpdate(req.params.id, {$set: req.body});
+			res.status(200).json("Account has been updated");
+		} catch(err) {
+			res.status(500).json(err)
+		}
+	} else {
+		return res.status(403).json("You can only update your own profile")
+	}
+});
+
+// Delete User
+router.delete('/:id', async (req, res)=> {
+	if(req.body.userId === req.params.id || req.body.isAdmin === true) {
+		try {
+			await userModel.findByIdAndDelete(req.params.id);
+			res.status(200).json("Account has been deleted!");
+		} catch(err) {
+			res.status(500).json(err);
+		}
+	} else {
+		return res.status(500).json("You only delete your own account!");
+	}
+});
+
 // Retrieve All User
 router.get("/allUser", async (req, res)=> {
 	try {
 		const retrieveUsers = await userModel.find({})
-		const {password, updatedAt, isAdmin, ...other} = retrieveUser._doc
-		res.status(200).json(other);
+		res.status(200).json(retrieveUsers);
 	} catch(err) {
 		res.status(500).json(err)
 	}
@@ -67,5 +102,6 @@ router.put('/:id/unfollow', async (req, res)=> {
 		}
 	}
 });
+
 
 module.exports = router;
